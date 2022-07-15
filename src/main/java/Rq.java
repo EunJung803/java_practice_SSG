a297e47bfd2eda59ab8fcb07c093b92e84ff711e
EunJung803 <aimee3322@gmail.com>
2022-07-15 16:40:53 +0900

    Rq class Refactoring

--- src/main/java/Rq.java
+++ src/main/java/Rq.java
@@ -1,30 +1,56 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Rq {
     String url;
+    String path;
+    Map<String, String> queryParams; // Map 선언
 
-    public Rq(String url) {
+    // getIntParam과 getPath 할때마다 ?을 기준으로 쪼개야 하기 때문에 두 부분에 다 존재
+    // 따라서 생성자을 통해 한번 받았을 때 쪼개주어 리팩토링 진행
+    public Rq(String url) {  // Rq 생성자
         this.url = url;
+        String[] urlBits = url.split("\\?", 2);  // 미리 쪼개놓는다 ~
+
+        this.path = urlBits[0];
+
+        queryParams = new HashMap<>();  // HashMap 생성
+
+        if(urlBits.length == 2) {  // ?를 써서 정보를 물어보지 않는 명령이 존재하기 때문에 조건문으로 걸러준다.
+            String queryStr = urlBits[1];
+
+            String[] paramBits = queryStr.split("&");
+
+            for (String paramBit : paramBits) {
+                String[] paramNameAndValue = paramBit.split("=", 2);
+
+                if (paramNameAndValue.length == 1) {
+                    continue;
+                }
+
+                String paramName = paramNameAndValue[0].trim();
+                String paramValue = paramNameAndValue[1].trim();
+
+                queryParams.put(paramName, paramValue);
+            }
+        }
     }
 
     public int getIntParam(String paramName, int defaultValue) {
-        String[] urlBits = url.split("\\?", 2);
-        urlBits = urlBits[1].split("&");
-
-        for (String urlBit : urlBits) {
-            String[] paramNameAndValue = urlBit.split("=", 2);
-            String paramName_ = paramNameAndValue[0];
-            String paramValue = paramNameAndValue[1];
-
-            if (paramName.equals(paramName_)) {
-                return Integer.parseInt(paramValue);
-            }
+        if (queryParams.containsKey(paramName) == false) {  // 값이 들어가있지 않으면
+            return defaultValue;
         }
 
-        return defaultValue;
+        String paramValue = queryParams.get(paramName);
+
+        if(paramValue.length() == 0) {
+            return defaultValue;
+        }
+
+        return Integer.parseInt(paramValue);
     }
 
     public String getPath() {
-        String[] urlBits = url.split("\\?", 2);
-
-        return urlBits[0];
+        return path;
     }
 }
