db0696ce75afcfe693d51991ee1626775cc93c48
EunJung803 <aimee3322@gmail.com>
2022-07-15 14:48:11 +0900

    TDD - Scanner의 input을 String으로 받기 Test

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,9 +12,22 @@ public class AppTest {
 
     @Test
     public void 스캐너에_키보드가_아닌_문자열을_입력으로_설정() {
-        Scanner sc = TestUtil.genScanner("안녕");
-        String cmd = sc.nextLine().trim();
-        assertEquals("안녕", cmd);
+        String input = """
+                등록
+                명언
+                작가
+                """.stripIndent();
+
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+
+        Scanner sc = new Scanner(in);
+        String cmd1 = sc.nextLine().trim();
+        String quote = sc.nextLine().trim();
+        String author = sc.nextLine().trim();
+
+        assertEquals("등록", cmd1);
+        assertEquals("명언", quote);
+        assertEquals("작가", author);
     }
 
     @Test
