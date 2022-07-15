2c63e5f5d9ad7dba4505cae3089faa23f15e7871
EunJung803 <aimee3322@gmail.com>
2022-07-15 14:02:21 +0900

    setting

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    void testing() {
+        String rs = "1";
+        assertEquals("1", rs);
+    }
+}
