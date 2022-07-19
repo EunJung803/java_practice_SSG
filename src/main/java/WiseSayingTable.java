757aca76e225a10baf79a54999d98897fba25cc6
EunJung803 <aimee3322@gmail.com>
2022-07-20 04:21:27 +0900

    객체를 파일로 저장할 때 자동 넘버링

--- src/main/java/WiseSayingTable.java
+++ src/main/java/WiseSayingTable.java
@@ -10,4 +10,27 @@ public class WiseSayingTable {
         String body = "내용";
         Util.file.saveToFile("%s/wise_saying/%d.json".formatted(baseDir, wiseSaying.id), body);
     }
+
+    public void save(String quote, String author) {
+        int id = getLastId() + 1;
+        
+        WiseSaying wiseSaying = new WiseSaying(id, quote, author);
+        save(wiseSaying);
+
+        saveLastId(id);
+    }
+
+    private void saveLastId(int id) {
+        Util.file.saveToFile("%s/wise_saying/last_id.txt".formatted(baseDir), id + "");
+    }
+
+    private int getLastId() {
+        String lastId = Util.file.readFromFile("%s/wise_saying/last_id.txt".formatted(baseDir), "");
+
+        if (lastId.isEmpty()) {
+            return 0;
+        }
+
+        return Integer.parseInt(lastId);
+    }
 }
--- src/test/java/WiseSayingTableTest.java
+++ src/test/java/WiseSayingTableTest.java
@@ -1,14 +1,35 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class WiseSayingTableTest {
     @Test
-    public void 등록을_하면_명언과_작가를_물어본다() {
+    public void 저장() {
         WiseSayingTable wiseSayingTable = new WiseSayingTable("test_data");
-        WiseSaying wiseSaying = new WiseSaying(1, "나에게 불가능이란 없다", "나폴레옹");
-        wiseSayingTable.save(wiseSaying);
 
+        wiseSayingTable.save("나에게 불가능이란 없다", "나폴레옹");
         assertTrue(new File("test_data/wise_saying/1.json").exists());
+
+        wiseSayingTable.save("내 죽음을 적들에게 알리지 마라", "이순신 장군");
+        assertTrue(new File("test_data/wise_saying/2.json").exists());
     }
+
+//    @Test
+//    public void 번호_없이_들어왔을때_자동넘버링_해주기() {
+//        WiseSayingTable wiseSayingTable = new WiseSayingTable("test_data");
+//        wiseSayingTable.save("나에게 불가능이란 없다", "나폴레옹");
+//
+//        assertTrue(new File("test_data/wise_saying/1.json").exists());
+//    }
+//
+//    @Test
+//    public void 등록을_하면_명언과_작가를_물어본다() {
+//        WiseSayingTable wiseSayingTable = new WiseSayingTable("test_data");
+//        WiseSaying wiseSaying = new WiseSaying(1, "나에게 불가능이란 없다", "나폴레옹");
+//        wiseSayingTable.save(wiseSaying);
+//
+//        assertTrue(new File("test_data/wise_saying/1.json").exists());
+//    }
 }
